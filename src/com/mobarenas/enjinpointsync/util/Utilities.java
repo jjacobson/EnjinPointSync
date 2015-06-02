865d3b6192602f7cf2c86a983efbd7c3833a84b1
BuildTools <j.jacobson966@gmail.com>
2015-06-01 21:43:29 -0800

    Use org.bukkit, not md5

--- src/com/mobarenas/enjinpointsync/util/Utilities.java
+++ src/com/mobarenas/enjinpointsync/util/Utilities.java
@@ -1,7 +1,7 @@
 package com.mobarenas.enjinpointsync.util;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Utilities {
